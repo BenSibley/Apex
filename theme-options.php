291d58c758e498ca1ed0609a87f8ac44bd20d3e2
BenSibley <ben@competethemes.com>
2015-05-28 09:34:53 -0400

    updated theme options page to use 'apex' instead of 'unlimited'

--- theme-options.php
+++ theme-options.php
@@ -1,59 +1,59 @@
 <?php
 
 /* create theme options page */
-function ct_unlimited_register_theme_page(){
-	add_theme_page( 'Apex Dashboard', 'Apex Dashboard', 'edit_theme_options', 'unlimited-options', 'ct_unlimited_options_content', 'ct_unlimited_options_content');
+function ct_apex_register_theme_page(){
+	add_theme_page( 'Apex Dashboard', 'Apex Dashboard', 'edit_theme_options', 'apex-options', 'ct_apex_options_content', 'ct_apex_options_content');
 }
-add_action( 'admin_menu', 'ct_unlimited_register_theme_page' );
+add_action( 'admin_menu', 'ct_apex_register_theme_page' );
 
 /* callback used to add content to options page */
-function ct_unlimited_options_content(){
+function ct_apex_options_content(){
 	?>
-	<div id="unlimited-dashboard-wrap" class="wrap">
-		<h2><?php _e('Apex Dashboard', 'unlimited'); ?></h2>
+	<div id="apex-dashboard-wrap" class="wrap">
+		<h2><?php _e('Apex Dashboard', 'apex'); ?></h2>
 		<?php hybrid_do_atomic( 'theme_options_before' ); ?>
 		<div class="content content-customization">
-			<h3><?php _e('Customization', 'unlimited'); ?></h3>
-			<p><?php _e('Click the "Customize" link in your menu, or use the button below to get started customizing Apex', 'unlimited'); ?>.</p>
+			<h3><?php _e('Customization', 'apex'); ?></h3>
+			<p><?php _e('Click the "Customize" link in your menu, or use the button below to get started customizing Apex', 'apex'); ?>.</p>
 			<p>
-				<a class="button-primary" href="<?php echo admin_url('customize.php'); ?>"><?php _e('Use Customizer', 'unlimited') ?></a>
+				<a class="button-primary" href="<?php echo admin_url('customize.php'); ?>"><?php _e('Use Customizer', 'apex') ?></a>
 			</p>
 		</div>
 		<div class="content content-support">
-			<h3><?php _e('Support', 'unlimited'); ?></h3>
-			<p><?php _e("You can find the knowledgebase, changelog, support forum, and more in the Apex Support Center", "unlimited"); ?>.</p>
+			<h3><?php _e('Support', 'apex'); ?></h3>
+			<p><?php _e("You can find the knowledgebase, changelog, support forum, and more in the Apex Support Center", "apex"); ?>.</p>
 			<p>
-				<a target="_blank" class="button-primary" href="https://www.competethemes.com/documentation/unlimited-support-center/"><?php _e('Visit Support Center', 'unlimited'); ?></a>
+				<a target="_blank" class="button-primary" href="https://www.competethemes.com/documentation/apex-support-center/"><?php _e('Visit Support Center', 'apex'); ?></a>
 			</p>
 		</div>
 		<div class="content content-premium-upgrade">
-			<h3><?php _e('Upgrade to Apex Pro', 'unlimited'); ?></h3>
-			<p><?php _e('Apex Pro is the premium upgrade for Apex. It has custom colors, new layouts, background images, and more', 'unlimited'); ?>...</p>
+			<h3><?php _e('Upgrade to Apex Pro', 'apex'); ?></h3>
+			<p><?php _e('Apex Pro is the premium upgrade for Apex. It has custom colors, new layouts, background images, and more', 'apex'); ?>...</p>
 			<p>
-				<a target="_blank" class="button-primary" href="https://www.competethemes.com/unlimited-pro/"><?php _e('See Full Feature List', 'unlimited'); ?></a>
+				<a target="_blank" class="button-primary" href="https://www.competethemes.com/apex-pro/"><?php _e('See Full Feature List', 'apex'); ?></a>
 			</p>
 		</div>
 		<div class="content content-resources">
-			<h3><?php _e('WordPress Resources', 'unlimited'); ?></h3>
-			<p><?php _e('Save time and money searching for WordPress products by following our recommendations', 'unlimited'); ?>.</p>
+			<h3><?php _e('WordPress Resources', 'apex'); ?></h3>
+			<p><?php _e('Save time and money searching for WordPress products by following our recommendations', 'apex'); ?>.</p>
 			<p>
-				<a target="_blank" class="button-primary" href="https://www.competethemes.com/wordpress-resources/"><?php _e('View Resources', 'unlimited'); ?></a>
+				<a target="_blank" class="button-primary" href="https://www.competethemes.com/wordpress-resources/"><?php _e('View Resources', 'apex'); ?></a>
 			</p>
 		</div>
 		<div class="content content-delete-settings">
-			<h3><?php _e('Reset Customizer Settings', 'unlimited'); ?></h3>
+			<h3><?php _e('Reset Customizer Settings', 'apex'); ?></h3>
 			<p>
 				<?php
 				$url = admin_url('customize.php');
-				$text = sprintf( __( '<strong>Warning:</strong> Clicking this button will erase your current settings in the <a href="%s">Customizer</a>', 'unlimited' ), esc_url( $url ) );
+				$text = sprintf( __( '<strong>Warning:</strong> Clicking this button will erase your current settings in the <a href="%s">Customizer</a>', 'apex' ), esc_url( $url ) );
 				echo $text . ".";
 				?>
 			</p>
 			<form method="post">
-				<input type="hidden" name="unlimited_reset_customizer" value="unlimited_reset_customizer_settings" />
+				<input type="hidden" name="apex_reset_customizer" value="apex_reset_customizer_settings" />
 				<p>
-					<?php wp_nonce_field( 'unlimited_reset_customizer_nonce', 'unlimited_reset_customizer_nonce' ); ?>
-					<?php submit_button( __( 'Reset Customizer Settings', 'unlimited' ), 'delete', 'delete', false ); ?>
+					<?php wp_nonce_field( 'apex_reset_customizer_nonce', 'apex_reset_customizer_nonce' ); ?>
+					<?php submit_button( __( 'Reset Customizer Settings', 'apex' ), 'delete', 'delete', false ); ?>
 				</p>
 			</form>
 		</div>
