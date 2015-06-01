e77938884721c3d7cde92bb2429fe630645f9b7c
BenSibley <ben@competethemes.com>
2015-06-01 08:22:14 -0400

    1 comment instead of One comment

--- content/comments-link.php
+++ content/comments-link.php
@@ -2,10 +2,10 @@
 	<i class="fa fa-comment" title="<?php _e('comment icon', 'apex'); ?>"></i>
 	<?php
 	if( ! comments_open() && get_comments_number() < 1 ) :
-		comments_number( __( 'Comments closed', 'apex' ), __( 'One Comment', 'apex'), __( '% Comments', 'apex' ) );
+		comments_number( __( 'Comments closed', 'apex' ), __( '1 Comment', 'apex'), __( '% Comments', 'apex' ) );
 	else :
 		echo '<a href="' . get_comments_link() . '">';
-			comments_number( __( 'Leave a Comment', 'apex' ), __( 'One Comment', 'apex'), __( '% Comments', 'apex' ) );
+			comments_number( __( 'Leave a Comment', 'apex' ), __( '1 Comment', 'apex'), __( '% Comments', 'apex' ) );
 		echo '</a>';
 	endif;
 	?>
