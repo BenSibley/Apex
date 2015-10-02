edabda0f856814cba0739a5e0d9cf2f47bd61e7a
BenSibley <ben@competethemes.com>
2015-10-02 07:58:33 -0400

    added grunt shell command

--- gruntfile.js
+++ gruntfile.js
@@ -122,6 +122,22 @@ module.exports = function(grunt) {
                 bootstrap: 'tests/php/phpunit.php',
                 colors: true
             }
+        },
+        excludeFiles: '--exclude "*.gitignore" --exclude ".sass-cache/" --exclude "*.DS_Store" --exclude ".git/" --exclude ".idea/" --exclude "gruntfile.js" --exclude "node_modules/" --exclude "package.json" --exclude "sass/"',
+        shell: {
+            zip: {
+                command: [
+                    // delete existing copies (if they exist)
+                    'rm -R /Users/bensibley/Documents/compete-themes/dist/apex || true',
+                    'rm -R /Users/bensibley/Documents/compete-themes/dist/apex.zip || true',
+                    // copy plugin folder without any project/meta files
+                    'rsync -r /Applications/MAMP/htdocs/wordpress/wp-content/themes/apex /Users/bensibley/Documents/compete-themes/dist/ <%= excludeFiles %>',
+                    // open dist folder
+                    'cd /Users/bensibley/Documents/compete-themes/dist/',
+                    // zip the apex folder
+                    'zip -r apex.zip apex'
+                ].join('&&')
+            }
         }
     });
 
@@ -137,8 +153,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-phpcs');
     grunt.loadNpmTasks('grunt-phpunit');
     grunt.loadNpmTasks('grunt-cssjanus');
+    grunt.loadNpmTasks('grunt-shell');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'watch', 'sass', 'autoprefixer', 'cssmin', 'compress', 'makepot', 'phpcs', 'phpunit', 'cssjanus']);
+    grunt.registerTask('default', ['concat', 'uglify', 'watch', 'sass', 'autoprefixer', 'cssmin', 'compress', 'makepot', 'phpcs', 'phpunit', 'cssjanus', 'shell']);
 
 };
