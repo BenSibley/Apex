5274c64c27bb8384be8de2f2325d20657f1feca8
BenSibley <ben@competethemes.com>
2015-10-28 10:05:53 -0400

    added 'for' attribute to comment form labels

--- functions.php
+++ functions.php
@@ -126,21 +126,21 @@ if( ! function_exists( 'apex_update_fields' ) ) {
 
         $fields['author'] =
             '<p class="comment-form-author">
-	            <label>' . __( "Name", "apex" ) . $label . '</label>
+	            <label for="author">' . __( "Name", "apex" ) . $label . '</label>
 	            <input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
             '" size="30" ' . $aria_req . ' />
 	        </p>';
 
         $fields['email'] =
             '<p class="comment-form-email">
-	            <label>' . __( "Email", "apex" ) . $label . '</label>
+	            <label for="email">' . __( "Email", "apex" ) . $label . '</label>
 	            <input id="email" name="email" type="email" value="' . esc_attr( $commenter['comment_author_email'] ) .
             '" size="30" ' . $aria_req . ' />
 	        </p>';
 
         $fields['url'] =
             '<p class="comment-form-url">
-	            <label>' . __( "Website", "apex" ) . '</label>
+	            <label for="url">' . __( "Website", "apex" ) . '</label>
 	            <input id="url" name="url" type="url" value="' . esc_attr( $commenter['comment_author_url'] ) .
             '" size="30" />
 	            </p>';
@@ -155,7 +155,7 @@ if( ! function_exists( 'apex_update_comment_field' ) ) {
 
         $comment_field =
             '<p class="comment-form-comment">
-	            <label>' . __( "Comment", "apex" ) . '</label>
+	            <label for="comment">' . __( "Comment", "apex" ) . '</label>
 	            <textarea required id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>
 	        </p>';
 
