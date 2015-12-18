d6222fe68fcd29a1fb8b46e412ec68f551b05ab3
BenSibley <ben@competethemes.com>
2015-12-17 20:50:10 -0500

    index.php logic moved into a function

--- index.php
+++ index.php
@@ -5,40 +5,14 @@ get_template_part('content/archive-header');
 ?>
 
 <div id="loop-container" class="loop-container">
-
     <?php
-    // The loop
     if ( have_posts() ) :
         while (have_posts() ) :
             the_post();
-
-            /* Blog */
-            if( is_home() ) {
-                get_template_part( 'content', 'archive' );
-            }
-            /* Post */
-            elseif( is_singular( 'post' ) ) {
-                get_template_part( 'content' );
-            }
-            /* Page */
-            elseif( is_page() ) {
-                get_template_part( 'content', 'page' );
-            }
-            /* Attachment */
-            elseif( is_attachment() ) {
-                get_template_part( 'content', 'attachment' );
-            }
-            /* Archive */
-            elseif( is_archive() ) {
-                get_template_part( 'content', 'archive' );
-            }
-            /* Custom Post Type */
-            else {
-                get_template_part( 'content' );
-            }
+            ct_apex_get_content_template();
         endwhile;
-    endif; ?>
-
+    endif;
+    ?>
 </div>
 
 <?php echo ct_apex_loop_pagination(); ?>
