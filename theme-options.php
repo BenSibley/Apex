1534e8579d708966a5ba774f4f036aee2f8f7ebf
BenSibley <ben@competethemes.com>
2015-12-17 20:53:09 -0500

    added use of add_query_arg where relevant

--- theme-options.php
+++ theme-options.php
@@ -12,7 +12,7 @@ function ct_apex_options_content(){
 	$customizer_url = add_query_arg(
 		array(
 			'url'    => site_url(),
-			'return' => admin_url('themes.php?page=apex-options')
+			'return' => add_query_arg( 'page', 'apex-options', admin_url( 'themes.php' ) )
 		),
 		admin_url('customize.php')
 	);
