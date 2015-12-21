ef1b67da0f8f20364e4dee6cabea2455f4133c75
BenSibley <ben@competethemes.com>
2015-12-21 14:35:12 -0500

    styled search bar and added opening functionality

--- js/functions.js
+++ js/functions.js
@@ -15,13 +15,14 @@ jQuery(document).ready(function($){
     removeToggleDropdownKeyboard();
     objectFitAdjustment();
 
+    toggleNavigation.on('click', openPrimaryMenu);
+    toggleDropdown.on('click', openDropdownMenu);
+    body.on('click', '#search-icon', openSearchBar);
+
     $(window).resize(function(){
         removeToggleDropdownKeyboard();
         objectFitAdjustment();
     });
-
-    toggleNavigation.on('click', openPrimaryMenu);
-    toggleDropdown.on('click', openDropdownMenu);
 
     $('.post-content').fitVids({
         customSelector: 'iframe[src*="dailymotion.com"], iframe[src*="slideshare.net"], iframe[src*="animoto.com"], iframe[src*="blip.tv"], iframe[src*="funnyordie.com"], iframe[src*="hulu.com"], iframe[src*="ted.com"], iframe[src*="wordpress.tv"]'
@@ -174,6 +175,39 @@ jQuery(document).ready(function($){
                     }
                 }
             });
+        }
+    }
+
+    function openSearchBar(){
+
+        if( $(this).hasClass('open') ) {
+
+            $(this).removeClass('open');
+            socialMediaIcons.removeClass('fade');
+
+            // make search input inaccessible to keyboards
+            siteHeader.find('.search-field').attr('tabindex', -1);
+
+            // handle mobile width search bar sizing
+            if( window.innerWidth < 900 ) {
+                siteHeader.find('.search-form').attr('style', '');
+            }
+        } else {
+
+            $(this).addClass('open');
+            socialMediaIcons.addClass('fade');
+
+            // make search input keyboard accessible
+            siteHeader.find('.search-field').attr('tabindex', 0);
+
+            // handle mobile width search bar sizing
+            if( window.innerWidth < 800 ) {
+
+                // distance to other side (35px is width of icon space)
+                var leftDistance = window.innerWidth * 0.83332 - 35;
+
+                siteHeader.find('.search-form').css('left', -leftDistance + 'px')
+            }
         }
     }
 });
