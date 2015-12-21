abbe140ad3e08f13ece79eca21e2f80c49de37df
BenSibley <ben@competethemes.com>
2015-12-21 15:01:11 -0500

    corrected user capability check for resetting theme mods

--- functions.php
+++ functions.php
@@ -459,7 +459,7 @@ function ct_apex_reset_customizer_options() {
 		return;
 
 	// validate user permissions
-	if( ! current_user_can( 'manage_options' ) )
+	if( ! current_user_can( 'edit_theme_options' ) )
 		return;
 
 	// delete customizer mods
