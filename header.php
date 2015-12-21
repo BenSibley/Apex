89a16cb2dc8caafd41ea39bdc4de27b4a183bfda
BenSibley <ben@competethemes.com>
2015-12-21 17:58:32 -0500

    support for Yoast breadcrumbs added

--- header.php
+++ header.php
@@ -3,34 +3,25 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-
     <?php wp_head(); ?>
-
 </head>
 
 <body id="<?php print get_stylesheet(); ?>" <?php body_class(); ?>>
-
 	<?php do_action( 'body_top' ); ?>
-
-	<!--skip to content link-->
 	<a class="skip-content" href="#main"><?php _e('Skip to content', 'apex'); ?></a>
-
 	<div id="overflow-container" class="overflow-container">
 		<div id="max-width" class="max-width">
 			<?php do_action( 'before_header' ); ?>
 			<header class="site-header" id="site-header" role="banner">
-
 				<div id="menu-primary-container" class="menu-primary-container">
 					<?php get_template_part( 'menu', 'primary' ); ?>
 					<?php get_template_part( 'content/search-bar' ); ?>
 					<?php ct_apex_social_icons_output('header'); ?>
 				</div>
-
 				<button id="toggle-navigation" class="toggle-navigation">
 					<span class="screen-reader-text"><?php _e('open menu', 'apex'); ?></span>
 					<?php echo ct_apex_svg_output( 'toggle-navigation' ); ?>
 				</button>
-
 				<div id="title-container" class="title-container">
 					<?php get_template_part('logo')  ?>
 					<?php if ( get_bloginfo( 'description' ) ) {
@@ -40,4 +31,7 @@
 			</header>
 			<?php do_action( 'after_header' ); ?>
 			<section id="main" class="main" role="main">
-				<?php do_action( 'main_top' ); ?>
+				<?php do_action( 'main_top' );
+				if ( function_exists( 'yoast_breadcrumb' ) ) {
+					yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
+				}
