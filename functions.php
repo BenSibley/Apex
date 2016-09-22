196e86bbe0d0f9c49fc51bbbd81afffc00a636c7
BenSibley <ben@competethemes.com>
2016-09-22 08:43:30 -0400

    switched any use of esc_attr outside of attributes to esc_html

--- functions.php
+++ functions.php
@@ -352,7 +352,7 @@ if ( ! function_exists( 'ct_apex_social_icons_output' ) ) {
 							<a class="email" target="_blank"
 							   href="mailto:<?php echo antispambot( is_email( get_theme_mod( $key ) ) ); ?>">
 								<i class="fa fa-envelope" title="<?php esc_attr_e( 'email', 'apex' ); ?>"></i>
-								<span class="screen-reader-text"><?php esc_attr_e('email', 'apex'); ?></span>
+								<span class="screen-reader-text"><?php esc_html_e('email', 'apex'); ?></span>
 							</a>
 						</li>
 					<?php } elseif ( $active_site == 'skype' ) { ?>
@@ -360,7 +360,7 @@ if ( ! function_exists( 'ct_apex_social_icons_output' ) ) {
 							<a class="<?php echo esc_attr( $active_site ); ?>" target="_blank"
 							   href="<?php echo esc_url( get_theme_mod( $key ), array( 'http', 'https', 'skype' ) ); ?>">
 								<i class="<?php echo esc_attr( $class ); ?>" title="<?php echo esc_attr( $active_site ); ?>"></i>
-								<span class="screen-reader-text"><?php echo esc_attr( $active_site );  ?></span>
+								<span class="screen-reader-text"><?php echo esc_html( $active_site );  ?></span>
 							</a>
 						</li>
 					<?php } else { ?>
@@ -368,7 +368,7 @@ if ( ! function_exists( 'ct_apex_social_icons_output' ) ) {
 							<a class="<?php echo esc_attr( $active_site ); ?>" target="_blank"
 							   href="<?php echo esc_url( get_theme_mod( $key ) ); ?>">
 								<i class="<?php echo esc_attr( $class ); ?>" title="<?php echo esc_attr( $active_site ); ?>"></i>
-								<span class="screen-reader-text"><?php echo esc_attr( $active_site );  ?></span>
+								<span class="screen-reader-text"><?php echo esc_html( $active_site );  ?></span>
 							</a>
 						</li>
 						<?php
