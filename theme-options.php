2bcc825e5bd7a5ac31d4afaad5f9b59aaebfd33c
BenSibley <ben@competethemes.com>
2017-02-22 11:10:29 -0500

    switched use of site_url() to get_home_url()

--- theme-options.php
+++ theme-options.php
@@ -9,7 +9,7 @@ function ct_apex_options_content() {
 
 	$customizer_url = add_query_arg(
 		array(
-			'url'    => site_url(),
+			'url'    => get_home_url(),
 			'return' => add_query_arg( 'page', 'apex-options', admin_url( 'themes.php' ) )
 		),
 		admin_url( 'customize.php' )
