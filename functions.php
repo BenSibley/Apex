da5044f1ed6ff46bb45bf741950fd3cf3b727a7a
BenSibley <ben@competethemes.com>
2017-05-24 13:42:43 -0400

    custom excerpts no longer outputting ellipsis

--- functions.php
+++ functions.php
@@ -150,7 +150,7 @@ if ( ! function_exists( 'ct_apex_remove_comments_notes_after' ) ) {
 add_action( 'comment_form_defaults', 'ct_apex_remove_comments_notes_after' );
 
 if ( ! function_exists( 'ct_apex_filter_read_more_link' ) ) {
-	function ct_apex_filter_read_more_link() {
+	function ct_apex_filter_read_more_link( $custom = false ) {
 		global $post;
 		$ismore             = strpos( $post->post_content, '<!--more-->' );
 		$read_more_text     = get_theme_mod( 'read_more_text' );
@@ -159,7 +159,7 @@ if ( ! function_exists( 'ct_apex_filter_read_more_link' ) ) {
 		$output = '';
 
 		// add ellipsis for automatic excerpts
-		if ( empty( $ismore ) ) {
+		if ( empty( $ismore ) && $custom !== true ) {
 			$output .= $excerpt_more;
 		}
 		// Because i18n text cannot be stored in a variable
@@ -179,7 +179,7 @@ if ( ! function_exists( 'ct_apex_filter_manual_excerpts' ) ) {
 	function ct_apex_filter_manual_excerpts( $excerpt ) {
 		$excerpt_more = '';
 		if ( has_excerpt() ) {
-			$excerpt_more = ct_apex_filter_read_more_link();
+			$excerpt_more = ct_apex_filter_read_more_link( true );
 		}
 		return $excerpt . $excerpt_more;
 	}
