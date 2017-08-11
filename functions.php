346cc546d21471239370d3a25b96f7282fdd0c10
BenSibley <ben@competethemes.com>
2017-08-11 12:21:17 -0400

    fix for yoast seo facebook description

--- functions.php
+++ functions.php
@@ -239,6 +239,18 @@ if ( ! function_exists( 'ct_apex_remove_more_link_scroll' ) ) {
 }
 add_filter( 'the_content_more_link', 'ct_apex_remove_more_link_scroll' );
 
+// Yoast OG description has "Continue readingTitle of the Post" due to its use of get_the_excerpt(). This fixes that.
+function ct_apex_update_yoast_og_description( $ogdesc ) {
+	$read_more_text = get_theme_mod( 'read_more_text' );
+	if ( empty( $read_more_text ) ) {
+		$read_more_text = __( 'Continue reading', 'apex' );
+	}
+	$ogdesc = substr( $ogdesc, 0, strpos( $ogdesc, $read_more_text ) );
+
+	return $ogdesc;
+}
+add_filter( 'wpseo_opengraph_desc', 'ct_apex_update_yoast_og_description' );
+
 if ( ! function_exists( 'ct_apex_featured_image' ) ) {
 	function ct_apex_featured_image() {
 
