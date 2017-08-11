d1c9721178a9f21ecfdbd1e3609d11a2a1b5e069
BenSibley <ben@competethemes.com>
2017-08-11 12:21:47 -0400

    no longer showing sticky status on search results pages

--- functions.php
+++ functions.php
@@ -445,7 +445,7 @@ add_filter( 'walker_nav_menu_start_el', 'ct_apex_nav_dropdown_buttons', 10, 4 );
 if ( ! function_exists( ( 'ct_apex_sticky_post_marker' ) ) ) {
 	function ct_apex_sticky_post_marker() {
 
-		if ( is_sticky() && ! is_archive() ) {
+		if ( is_sticky() && !is_archive() && !is_search() ) {
 			echo '<div class="sticky-status"><span>' . __( "Featured Post", "apex" ) . '</span></div>';
 		}
 	}
