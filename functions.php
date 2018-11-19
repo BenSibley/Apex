0df011b59364f126762fcd06a753439b4e520ba6
BenSibley <ben@competethemes.com>
2018-11-19 10:31:54 -0500

    Gutenberg back-end support

--- functions.php
+++ functions.php
@@ -55,6 +55,13 @@ if ( ! function_exists( ( 'ct_apex_theme_setup' ) ) ) {
 		add_theme_support( 'wc-product-gallery-lightbox' ); 
 		add_theme_support( 'wc-product-gallery-slider' );
 
+		// Gutenberg - wide & full images
+		add_theme_support( 'align-wide' );
+		add_theme_support( 'align-full' );
+
+		// Gutenberg - add support for editor styles
+		add_theme_support('editor-styles');
+
 		register_nav_menus( array(
 			'primary' => esc_html__( 'Primary', 'apex' )
 		) );
