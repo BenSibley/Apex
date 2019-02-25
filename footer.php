06352ca3ffc58136b9a2ce6661e2b1d1b461290f
BenSibley <ben@competethemes.com>
2019-02-25 10:42:42 -0500

    shortcodes can now be output in Footer Text added by Apex Pro

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
             <?php
             $footer_text = sprintf( __( '<a href="%1$s">%2$s WordPress Theme</a> by Compete Themes', 'apex' ), 'https://www.competethemes.com/apex/', wp_get_theme( get_template() ) );
             $footer_text = apply_filters( 'ct_apex_footer_text', $footer_text );
-            echo wp_kses_post( $footer_text );
+            echo do_shortcode( wp_kses_post( $footer_text ) );
             ?>
         </span>
 	</div>
