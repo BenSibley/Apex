8601d4773135e8622cac6cc94701ec5d55e02cb1
BenSibley <ben@competethemes.com>
2019-02-25 10:40:30 -0500

    WP 5.1 date fix

--- content/post-byline.php
+++ content/post-byline.php
@@ -1,7 +1,7 @@
 <div class="post-byline">
     <span class="post-date">
 		<?php
-		$date = date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) );
+		$date = date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'c' ) ) );
 		printf( esc_html_x( 'Published %s', 'Published on this date', 'apex' ), $date );
 		?>
 	</span>
