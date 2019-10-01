f7fe8ce2afcfecdf925d4d4e6182a33b32a2d88e
BenSibley <ben@competethemes.com>
2019-10-01 16:32:59 -0400

    removed activation redirect

--- functions.php
+++ functions.php
@@ -591,12 +591,6 @@ if ( ! function_exists( ( 'ct_apex_delete_settings_notice' ) ) ) {
 					<p><?php esc_html_e( 'Customizer settings deleted.', 'apex' ); ?></p>
 				</div>
 				<?php
-			} else if ( $_GET['apex_status'] == 'activated' ) {
-				?>
-				<div class="updated">
-					<p><?php printf( esc_html__( '%s successfully activated!', 'apex' ), wp_get_theme( get_template() ) ); ?></p>
-				</div>
-				<?php
 			}
 		}
 	}
@@ -736,20 +730,6 @@ if ( ! function_exists( ( 'ct_apex_allow_skype_protocol' ) ) ) {
 	}
 }
 add_filter( 'kses_allowed_protocols' , 'ct_apex_allow_skype_protocol' );
-
-// trigger theme switch on link click and send to Appearance menu
-function ct_apex_welcome_redirect() {
-
-	$welcome_url = add_query_arg(
-		array(
-			'page'        => 'apex-options',
-			'apex_status' => 'activated'
-		),
-		admin_url( 'themes.php' )
-	);
-	wp_safe_redirect( esc_url_raw( $welcome_url ) );
-}
-add_action( 'after_switch_theme', 'ct_apex_welcome_redirect' );
 
 if ( function_exists( 'ct_apex_pro_plugin_updater' ) ) {
 	remove_action( 'admin_init', 'ct_apex_pro_plugin_updater', 0 );
