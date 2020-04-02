678ed0601076622a3713e1529d91f4bed8a28d04
BenSibley <ben@competethemes.com>
2020-04-02 13:34:08 -0400

    email and phone icons now display contact info on hover

--- functions.php
+++ functions.php
@@ -459,7 +459,7 @@ if ( ! function_exists( 'ct_apex_social_icons_output' ) ) {
 						<li>
 							<a class="email" target="_blank"
 							   href="mailto:<?php echo antispambot( is_email( get_theme_mod( $key ) ) ); ?>">
-								<i class="fas fa-envelope" title="<?php echo esc_attr_x( 'email', 'noun', 'apex' ); ?>"></i>
+								<i class="fas fa-envelope" title="<?php echo antispambot( is_email( get_theme_mod( $key ) ) ); ?>"></i>
 								<span class="screen-reader-text"><?php echo esc_html_x('email', 'noun', 'apex'); ?></span>
 							</a>
 						</li>
@@ -475,7 +475,7 @@ if ( ! function_exists( 'ct_apex_social_icons_output' ) ) {
 						<li>
 							<a class="<?php echo esc_attr( $active_site ); ?>" target="_blank"
 									href="<?php echo esc_url( get_theme_mod( $active_site ), array( 'tel' ) ); ?>">
-								<i class="<?php echo esc_attr( $class ); ?>"></i>
+								<i class="<?php echo esc_attr( $class ); ?>" title="<?php echo str_replace( 'tel:', '', esc_url( get_theme_mod( $active_site ), array( 'tel' ) ) ); ?>"></i>
 								<span class="screen-reader-text"><?php echo esc_html( $active_site );  ?></span>
 							</a>
 						</li>
