c362aea57c2ddbf61da6c0f436b4b69780789e8b
Ben Sibley <ben@competethemes.com>
2020-12-23 13:09:15 -0500

    added nofollow to footer link

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@ if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_
 	<div class="design-credit">
         <span>
             <?php
-            $footer_text = sprintf( __( '<a href="%1$s">%2$s WordPress Theme</a> by Compete Themes', 'apex' ), 'https://www.competethemes.com/apex/', wp_get_theme( get_template() ) );
+            $footer_text = sprintf( __( '<a href="%1$s" rel="nofollow">%2$s WordPress Theme</a> by Compete Themes', 'apex' ), 'https://www.competethemes.com/apex/', wp_get_theme( get_template() ) );
             $footer_text = apply_filters( 'ct_apex_footer_text', $footer_text );
             echo do_shortcode( wp_kses_post( $footer_text ) );
             ?>
